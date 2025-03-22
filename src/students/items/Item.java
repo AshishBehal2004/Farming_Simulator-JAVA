df4e4a7ce259752a2aff9a7ed8f3f9e53583463b
ASHISH BEHAL <165093620+AshishBehal2004@users.noreply.github.com>
2025-03-22 13:11:40 +1030

    Started the Assignment.
    Created private variables for age, maturation_age, death_age, monetary_value. as well as made a constructor  to initialize the objects, Made setter for Age.

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,25 @@
 package students.items;
 
 public class Item {
-
+	private int age = 0;
+	private int maturation_age = 0;
+	private int death_age = 0;
+	private int monetary_value = 0;
+	
+	//Constructor
+	public Item() 
+	{
+		this.age = age;
+		this.maturation_age = maturation_age;
+		this.death_age = death_age;
+		this.monetary_value = monetary_value;
+		
+	}
+	
+	//Setter for Age
+	public void setAge(int age)
+	{
+		this.age= age;
+	}
 	
 }
