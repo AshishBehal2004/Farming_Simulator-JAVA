5218b03b44bb9edb2ab04bea4ca5676e4d20a40a
Ashish Behal <behay002@mymail.unisa.edu.au>
2025-04-12 12:16:09 +0930

    run() method in farm class
    added variable such as fieldWidth,fieldHeight, balance, field. And intialize them in constructor.
    
    In run() method, made a boolean a variable, set to true. A while loop that runs until quit(bool variable is false).
    made a Scanner object which reads and takes user input, however its not fully done yet.

--- src/marking/Marker.java
+++ src/marking/Marker.java
@@ -35,21 +35,23 @@ public class Marker {
 		
 		
 	
-		Field testField = new Field(5, 5);
-		System.out.println(testField.get(3, 3)); //should print "."
-		System.out.println(testField); //should print the field
-		System.out.println(testField.getValue()); //should print 0
-		Apples a = new Apples();
-		a.setAge(5);
-		testField.plant(0, 0, a);
-		System.out.println(testField.getValue()); //should print 3
-		System.out.println(a.equals(testField.get(0, 0))); //should print true
-		testField.till(0, 0);
-		System.out.println(a.equals(testField.get(0, 0))); //should print false
-		System.out.println(testField.get(0, 0).equals(new Soil())); //should print true
-		System.out.println(testField.getSummary());
+//		Field testField = new Field(5, 5);
+//		System.out.println(testField.get(3, 3)); //should print "."
+//		System.out.println(testField); //should print the field
+//		System.out.println(testField.getValue()); //should print 0
+//		Apples a = new Apples();
+//		a.setAge(5);
+//		testField.plant(0, 0, a);
+//		System.out.println(testField.getValue()); //should print 3
+//		System.out.println(a.equals(testField.get(0, 0))); //should print true
+//		testField.till(0, 0);
+//		System.out.println(a.equals(testField.get(0, 0))); //should print false
+//		System.out.println(testField.get(0, 0).equals(new Soil())); //should print true
+//		System.out.println(testField.getSummary());
 		
-
+//		Field testfield = new Field(6,9);	
+		Farm testfarm = new Farm(6,9,10);
+		testfarm.run();
 		// Section THREE
 		// uncomment this section once you get to it
 		//remember, your farm should work on varies size fields (assuming all fields are less than 10x10)
--- src/students/Farm.java
+++ src/students/Farm.java
@@ -1,13 +1,42 @@
 package students;
-
+import students.Field;
+import java.util.Scanner;
 public class Farm {
 	
+	private int fieldWidth;
+	private int fieldHeight;
+	private int balance;
+	private Field field;
+
 	public Farm(int fieldWidth, int fieldHeight, int startingFunds)
 	{
+		this.fieldWidth = fieldWidth;
+		this.fieldHeight = fieldHeight;
+		this.balance = startingFunds;
+		this.field = new Field(fieldWidth,fieldHeight);
 	}
 	
 	public void run()
 	{
+		boolean quit = true;
+		while (quit != false)
+		{
+			Scanner user = new Scanner(System.in);
+			System.out.println(field.toString());
+			System.out.println("Bank Balance: $"+balance+"\n");
+			System.out.println("Enter your next action: ");
+			System.out.print("  t x y: ");
+			String sc = user.nextLine();
+			System.out.print("  h x y: ");
+			String sc1 = user.nextLine();
+			System.out.println("  p x y: ");
+			System.out.println("  s x y: ");
+			System.out.println("  w: ");
+			System.out.println("  q: ");
+			
+		}
+		
+		
 	}
 	
 }
