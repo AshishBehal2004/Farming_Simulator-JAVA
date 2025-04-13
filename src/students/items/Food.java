fffc75a40f7c294ee0bce3dfdbba2a9bacad5366
Ashish Behal <behay002@mymail.unisa.edu.au>
2025-04-13 16:56:23 +0930

    Finished commenting Food Class

--- src/students/items/Food.java
+++ src/students/items/Food.java
@@ -1,7 +1,13 @@
 package students.items;
-
+//The Food class here is an abstract child class which inherits 
+//all the methods and variables from its parent class that is Item
 public abstract class Food extends Item 
 {
+	//Food's constructor which have super constructor that calls 
+	//Item's constructor (which takes maturation_age, death_age and
+	//monetary_value as its arguments. This class is later used in 
+	//its child classes: Grain and Apple, to be able to inherit all 
+	// methods(specifically constructor) and variables from parent class(Food).
 	public Food(int maturation_age, int death_age, int monetary_value)
 	{
 		super(maturation_age, death_age, monetary_value);
