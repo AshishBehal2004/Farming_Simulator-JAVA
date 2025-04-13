6a3352b90739e86a9bf6e30fdb9a167d6fc629d9
Ashish Behal <behay002@mymail.unisa.edu.au>
2025-04-13 17:09:41 +0930

    Done commenting UntilledSoil class

--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -1,12 +1,17 @@
 package students.items;
 
+//UntilledSoil here is the child class that inherits everything from its pare
+//class(Item). This class is later on used in Field.
 public class UntilledSoil extends Item{
-	
+	//UntilledSoil constructor that has super constructor which calls Item class
+	//constructor in order to add necessary value in the arguments.
 	public UntilledSoil()
 	{
 		super(Integer.MAX_VALUE,Integer.MAX_VALUE,-1);
 	}
 
+	//toString method overriding its base class(Item) toString() method, and 
+	//returning(displaying) the '/' symbol
 	@Override
 	public String toString() {
 		return "/";
